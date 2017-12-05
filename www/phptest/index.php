bb843a2ec53367b23b70fc213789d1b115d1e703
Yura440 <30841061+Yura440@users.noreply.github.com>
2017-12-05 13:54:02 +0300

    Update 2.5

---
+++ www/phptest/index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <?php
+            include("menu.php");
+        ?>
+    </body>
+</html>
---
+++ www/phptest/menu.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Menu</title>
+    </head>
+    <body>
+        <div height="50px">
+            <a href="index.php">Home</a>
+            <a href="document.php">Document</a>
+        </div>
+        <style>
+            body{
+                backgrount-color: #000000;
+            }
+        </style>
+        </body>
+</html>
